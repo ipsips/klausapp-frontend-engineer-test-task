0f8f560b7a8ef7a5b8c8507e57fb48b5c98658cf
Ivar Oja <ips@ips.re>
2022-02-04 19:20:02 +0200

    switch to typescript

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,10 @@
 import { serve } from 'https://deno.land/std@0.114.0/http/server.ts';
 
 serve((_req) => {
+  /**
+   * Known issue: https://github.com/justjavac/typescript-deno-plugin/blob/v1.29.0/lib/lib.deno.d.ts#L2675
+   */
+  // @ts-ignore
   return new Response('Hello World!', {
     headers: { 'content-type': 'text/plain' },
   });
